1d3f80c5e15854b67e2ce896e84a31133b52d409
Jakob Schlyter <jakob@kirei.se>
2007-11-30 13:08:03 +0000

    tidy

--- util/locale2sql.pl
+++ util/locale2sql.pl
@@ -12,25 +12,25 @@ my ($hashref, $arrayref, $string) = LoadFile($file);
 
 my $lang = $hashref->{locale_id};
 
-foreach my $tag (keys %{$hashref->{messages}}) {
-	my $args = $hashref->{messages}{$tag}{args};
-	my $format = $hashref->{messages}{$tag}{format};
-	my $descr = $hashref->{messages}{$tag}{descr};
+foreach my $tag (keys %{ $hashref->{messages} }) {
+    my $args   = $hashref->{messages}{$tag}{args};
+    my $format = $hashref->{messages}{$tag}{format};
+    my $descr  = $hashref->{messages}{$tag}{descr};
 
-	if ($format) {
-			$format = sprintf("\"%s\"", $format);
-	} else {
-		$format = "NULL";
-	}
-	
-	if ($descr) {
-			$descr = sprintf("\"%s\"", $descr);
-	} else {
-		$descr = "NULL";
-	}
+    if ($format) {
+        $format = sprintf("\"%s\"", $format);
+    } else {
+        $format = "NULL";
+    }
 
-	printf("INSERT INTO messages ");
-	printf("(tag,arguments,language,formatstring,description) \n");
-	printf("VALUES (\"%s\",%d,\"%s\",%s,%s);\n",
-		$tag, $args, $lang, $format, $descr);
+    if ($descr) {
+        $descr = sprintf("\"%s\"", $descr);
+    } else {
+        $descr = "NULL";
+    }
+
+    printf("INSERT INTO messages ");
+    printf("(tag,arguments,language,formatstring,description) \n");
+    printf("VALUES (\"%s\",%d,\"%s\",%s,%s);\n",
+        $tag, $args, $lang, $format, $descr);
 }
