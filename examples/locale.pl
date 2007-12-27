0bfc1144cc8d91ec84b808783c82ac17ea374be4
Jakob Schlyter <jakob@kirei.se>
2007-12-27 09:04:59 +0000

    revert to old locale example

--- examples/locale.pl
+++ examples/locale.pl
@@ -8,17 +8,17 @@ use strict;
 
 use Data::Dumper;
 
-use DNSCheck;
+use DNSCheck::Logger;
+use DNSCheck::Lookup::DNS;
+use DNSCheck::Lookup::ASN;
 
 ######################################################################
 
-my $check = new DNSCheck(
-    {
-        interactive => 1,
-        locale      => "locale/en.yaml"
-    }
-);
+my $logger =
+  new DNSCheck::Logger({ interactive => 1, locale => "locale/en.yaml" });
+my $dns = new DNSCheck::Lookup::DNS($logger);
+my $asn = new DNSCheck::Lookup::ASN($logger, $dns);
 
 die "syntax error" unless ($ARGV[0]);
 
-$check->zone($ARGV[0]);
+$asn->lookup($ARGV[0]);
