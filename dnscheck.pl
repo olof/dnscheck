b56a822674867ccbb5b58bae0bac13001ddbcb87
Jakob Schlyter <jakob@kirei.se>
2008-02-26 09:19:06 +0000

    clean up and make almost user friendly

--- dnscheck.pl
+++ dnscheck.pl
@@ -6,22 +6,59 @@ require 5.8.0;
 use warnings;
 use strict;
 
-use Data::Dumper;
-
+use Getopt::Long;
+use Pod::Usage;
 use DNSCheck;
 
 ######################################################################
 
-my $check = new DNSCheck(
-    {
-        interactive  => 1,
-        locale       => "locale/en.yaml",
-        disable_ipv4 => 0,
-        disable_ipv6 => 0,
+sub main {
+    my $help    = 0;
+    my $timeout = 2;
+    my $debug   = 0;
+    my $locale  = "locale/en.yaml";
 
+    GetOptions(
+        'help|?'    => \$help,
+        'timeout=i' => \$timeout,
+        'debug'     => \$debug,
+    ) or pod2usage(2);
+    pod2usage(1) if ($help);
+
+    my $zone = shift @ARGV;
+
+    unless ($zone) {
+        pod2usage(2);
     }
-);
 
-die "usage: dnscheck.pl [zone]" unless ($ARGV[0]);
+    $locale = undef if ($debug);
 
-$check->zone($ARGV[0]);
+    my $check = new DNSCheck(
+        {
+            interactive => 1,
+            locale      => $locale,
+            udp_timeout => $timeout,
+            tcp_timeout => $timeout,
+        }
+    );
+
+    $check->zone($zone);
+}
+
+main();
+
+__END__
+
+=head1 NAME
+
+dnscheck - DNSCheck Tool
+
+=head1 SYNOPSIS
+
+dnscheck [options] zone
+
+Options:
+
+ --help                brief help message
+ --debug               debug
+ --timeout=SECONDS     set UDP/TCP timeout
