46f5785fe6ac133e9f56c3231f026b917c24581f
Mats Jernsell <matsjn@gmail.com>
2008-04-14 19:14:50 +0000

    Script for applying perltidy on svn modified files

---
+++ engine/util/svntidy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# $Id$
+
+TIDYRC=util/perltidyrc
+
+svn st -q | egrep '\.pl$|\.pm$' | awk '{print $2}' |\
+xargs perltidy --profile=${TIDYRC} --backup-and-modify-in-place
+find . \( -name '*.pl.bak' -o -name '*.pm.bak' \) -type f -print |\
+xargs rm
