9d08a267e6623d6ae5ab14f1964844ead078b9ef
Einar Lonn <einar.lonn@iis.se>
2008-06-27 14:21:50 +0000

    Forgot to remove google-analytics from /webui. Run this version instead, it's 0.62 but cleaned from google-analytics. Sorry. ;)

--- webui/public_html/index.php
+++ webui/public_html/index.php
@@ -138,13 +138,5 @@
 
 </div>
 
-<script src="http://www.google-analytics.com/urchin.js"
-type="text/javascript">
-</script>
-<script type="text/javascript">
-_uacct = "UA-2736468-7";
-urchinTracker();
-</script>
-
 </body>
 </html>
