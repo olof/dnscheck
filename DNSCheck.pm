451e264feabfdfe99e06cd47107bf75a880da68f
Mats Jernsell <matsjn@gmail.com>
2008-08-22 14:24:50 +0000

    Bump version before tagging...

--- DNSCheck.pm
+++ DNSCheck.pm
@@ -47,7 +47,7 @@ use DNSCheck::Test::DNSSEC;
 use DNSCheck::Test::Mail;
 use DNSCheck::Test::SMTP;
 
-our $VERSION = "0.62";
+our $VERSION = "0.65";
 
 ######################################################################
 
