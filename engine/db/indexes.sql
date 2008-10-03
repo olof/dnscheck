43061a806aa351ae3b54a59e73f48627499708e1
Calle Dybedahl <calle@init.se>
2008-10-03 14:01:57 +0000

    Do the checkin to the right branch...

---
+++ engine/db/indexes.sql
@@ -0,0 +1,6 @@
+create index tests_domain on tests (domain);
+create index results_test_id on results (test_id);
+create index queue_domain on queue (domain);
+create index tests_begin on tests (begin);
+create index domain_delegation_history on delegation_history (domain);
+create index domains_domain on domains (domain);
