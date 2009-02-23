c388e1f6a7923c5a5a1ffb41fea58e65348be72a
Calle Dybedahl <calle@init.se>
2009-02-23 13:54:58 +0000

    Argh

--- engine/apps/dnscheck-12hourmailer.pl
+++ engine/apps/dnscheck-12hourmailer.pl
@@ -81,7 +81,7 @@ sub tests_for_domain {
 
     foreach my $t (keys %$rref) {
         $rref->{$t}{formatted} =
-          $locale->expand(grep { $_ }
+          $locale->expand(grep { defined($_) }
               @{ $rref->{$t} }
               {qw(message arg0 arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9)});
     }
