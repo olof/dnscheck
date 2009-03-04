68b2355a5cb672d2817c6843f56a5871a9151d94
Calle Dybedahl <calle@init.se>
2009-03-04 08:15:14 +0000

    Add example script to look things up using the DNSCheck resolver.

---
+++ engine/examples/lookup.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+#
+# $Id: zone.pl 715 2009-03-04 07:59:46Z calle $
+
+require 5.008;
+use warnings;
+use strict;
+
+use DNSCheck;
+
+######################################################################
+
+my $r = DNSCheck->new->resolver;
+
+my ($name, $type, $class) = @ARGV;
+
+$class ||= 'IN';
+$type  ||= 'A';
+
+die "usage: $0 name [type [class]]\n" unless $name;
+
+my $p = $r->recurse($name,$type,$class);
+
+if ($p) {
+    $p->print
+} else {
+    print "No answer.\n"
+}
