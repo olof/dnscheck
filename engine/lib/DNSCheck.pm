bd65ea826d877a7d55abf3716be12a764023af3d
Calle Dybedahl <calle@init.se>
2009-03-06 08:42:12 +0000

    Bump version number to 0.91

--- engine/lib/DNSCheck.pm
+++ engine/lib/DNSCheck.pm
@@ -55,7 +55,7 @@ use DNSCheck::Lookup::Resolver;
 use DNSCheck::Lookup::ASN;
 use DNSCheck::Logger;
 
-our $VERSION     = "0.90";
+our $VERSION     = "0.91";
 our $SVN_VERSION = '$Revision$';
 
 ######################################################################
