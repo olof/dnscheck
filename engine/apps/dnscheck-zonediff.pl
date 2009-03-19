3a33e813af9199a4e0e44eb574e3ffc746bb9e2b
Calle Dybedahl <calle@init.se>
2009-03-19 13:11:54 +0000

    Yet another version of zonediff. This one uses very little RAM, but relies heavily on the output format from dig being constant and correctly sorted.

--- engine/apps/dnscheck-zonediff.pl
+++ engine/apps/dnscheck-zonediff.pl
@@ -1,272 +1,320 @@
 #!/usr/bin/perl
+#
+# $Id: dnscheck.pl 721 2009-03-04 15:27:27Z calle $
+#
+# Copyright (c) 2007 .SE (The Internet Infrastructure Foundation).
+#                    All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+######################################################################
 
-use strict;
 use warnings;
+use strict;
 
-use Carp;
-use Storable;
-use DNSCheck;
 use Getopt::Long;
+use DNSCheck;
 
-my $bootstrap;
+my %changed;
+my %dropped;
+my %added;
+
 my $debug;
+my $dc;
+my $config;
+my %flagdomain;
+my $filename;
+my $tsig;
+my $dig;
+my @servers;
+my $domain;
 
-sub download_zone {
-    my $conf = shift;
+my $newsuffix    = '.new';
+my $backupsuffix = '.bak';
 
-    my $dig  = $conf->{dig};
-    my $tsig = $conf->{tsig};
+sub setup {
+    GetOptions("debug" => \$debug);
+
+    print "Running in debug mode.\n" if $debug;
+
+    $dc     = DNSCheck->new;
+    $config = $dc->config->get("zonediff");
+    foreach my $domain (@{ $config->{flagdomain} }) {
+        $flagdomain{exist}{$domain} = 1;
+        print "Flag domain: $domain\n" if $debug;
+    }
+
+    $filename = $config->{datafile};
+    $tsig     = $config->{tsig};
     $tsig =~ s/ TSIG /:/;
-    my @servers = @{ $conf->{servers} };
-    my $domain  = $conf->{domain};
-    my @flagdomains;
+    $dig     = $config->{dig};
+    @servers = @{ $config->{servers} };
+    $domain  = $config->{domain};
 
-    @flagdomains = @{ $conf->{flagdomain} } if defined($conf->{flagdomain});
+    if ($debug) {
+        print "Data file: $filename\n";
+        print "TSIG: $tsig\n";
+        print "dig: $dig\n";
+        print "Servers: @servers\n";
+        print "Domain: $domain\n";
+    }
+}
 
-    my %new;
-    my %newns;
+sub fetch_new_zone {
+    my $success;
 
-    while (my $server = shift @servers) {
-        open my $pipe, '-|', $dig, $domain, '@' . $server, 'axfr', '-y', $tsig
-          or die "Failed to run dig: $!\n";
-        while (defined(my $line = <$pipe>)) {
-            next if $line =~ /^;/;
-            printf("%10d\r", $.) if $debug;
-            chomp($line);
-            if (
-                my ($name, $type, $rest) =
-                $line =~ m/
-            ^
-            ([-.a-z0-9]+)\. # Name
-            \s+
-            \d+           # TTL
-            \s+
-            IN
-            \s+
-            ((?:NS)|(?:DS)|(?:A))     # Type
-            \s+
-            (.+)          # Rest
-            $
-            /x
-              )
-            {
-                push @{ $new{$name}{$type} }, $rest;
-                if ($type eq 'NS') {
-                    $newns{$name}{$rest} = 1;
-                }
-
-            }
-        }
-        print "\n" if $debug;
-
-        if (@flagdomains
-            and
-            !(scalar(grep { $new{$_} } @flagdomains) == scalar(@flagdomains)))
-        {
-            warn "Incomplete transfer (no flag domain), trying next server.\n";
-            die "No more servers to try. Giving up.\n" if (@servers == 0);
-            next;
-        } else {
+    foreach my $server (@servers) {
+        print "Trying server $server...\n" if $debug;
+        my $res =
+          system("$dig axfr $domain \@$server -y $tsig > $filename$newsuffix");
+        $res >>= 8;
+        if ($res == 0) {
+            print "Got file.\n" if $debug;
+            $success = 1;
             last;
         }
     }
 
-    return \%new, \%newns;
-}
-
-# We want to test a domain if:
-#
-#  * a domain has been added
-#
-#  * a domain has had changes in NS
-#
-#  * a domain has had changes in DS
-
-sub get_changed_domains {
-    my $conf     = shift;
-    my $filename = $conf->{datafile};
-    my @flagdomains;
-    my %changed;
-    my %dropped;
-
-    @flagdomains = @{ $conf->{flagdomain} } if defined($conf->{flagdomain});
-
-    my ($new, $newns) = download_zone($conf);
-
-    my $old;
-    eval { $old = retrieve($filename); };
-    print "Old data loaded.\n" if $debug;
-
-    if (    @flagdomains
-        and !(scalar(grep { $old->{$_} } @flagdomains) == scalar(@flagdomains))
-        and !$bootstrap)
-    {
-        die "$filename corrupt. Giving up.\n";
-    }
-
-    rename $filename, $filename . '.bak';
-    store $new, $filename;
-
-    foreach my $domain (keys %$new) {
-        printf("%70s\r", $domain) if $debug;
-        if (!defined($old->{$domain})) {
-            $changed{$domain} = 'NEW';
-        } else {
-            my $o = $old->{$domain};
-            my $n = $new->{$domain};
-
-            next
-              if (  norm($o->{NS}) eq norm($n->{NS})
-                and norm($o->{DS}) eq norm($n->{DS})
-                and norm($o->{A})  eq norm($n->{A}));
-            $changed{$domain} = '';
-            $changed{$domain} .= 'NS ' if norm($o->{NS}) ne norm($n->{NS});
-            $changed{$domain} .= 'DS ' if norm($o->{DS}) ne norm($n->{DS});
-            $changed{$domain} .= 'A '  if norm($o->{A})  ne norm($n->{A});
+    unless (-e $filename) {
+        print "$filename missing.\n";
+        if ($success) {
+            print "Moving just fetched file to $filename and exiting.\n";
+            rename $filename . $newsuffix, $filename;
+            exit(1);
         }
     }
-    print "\n" if $debug;
+}
 
-    foreach my $domain (keys %$old) {
-        next unless $old->{$domain}{NS};
-        foreach my $ns (@{ $old->{$domain}{NS} }) {
-            unless ($newns->{$domain}{$ns}) {
-                printf("Adding %s for %s to list of dropped nameservers.\n",
-                    $ns, $domain)
-                  if $debug;
-                $dropped{$domain}{$ns} = 1;
+sub switch_files_around {
+    rename $filename, $filename . $backupsuffix
+      or die "Failed to rename save file to backup file: $!\n";
+    print "$filename renamed to $filename$backupsuffix\n" if $debug;
+    rename $filename . $newsuffix, $filename
+      or die "Failed to rename new file to save file: $!\n";
+    print "$filename$newsuffix renamed to $filename\n" if $debug;
+}
+
+sub check_for_flagdomains {
+    foreach my $name (keys %{ $flagdomain{exist} }) {
+        unless ($flagdomain{new}{$name}) {
+            die "Flag domain $name missing from new file. Exiting.\n";
+        }
+        unless ($flagdomain{old}{$name}) {
+            die "Flag domain $name missing from old file. Exiting.\n";
+        }
+    }
+}
+
+sub line_parse {
+    my ($line) = @_;
+
+    my ($name, $type, $rest) = $line =~ m/
+                ^
+                ([-.a-z0-9]+)\. # Name
+                \s+
+                \d+           # TTL
+                \s+
+                IN
+                \s+
+                ((?:NS)|(?:DS)|(?:A))     # Type
+                \s+
+                (.+)          # Rest
+                $
+                /x;
+    if ($name) {
+        return ($name, $type, $rest);
+    } else {
+        return;
+    }
+}
+
+sub get_iterator {
+    my $filename = shift;
+
+    my @buffer;
+    open my $fh, '<', $filename or die "$filename: $!\n";
+
+    return sub {
+        my @res;
+
+        while (defined(my $line = <$fh>)) {
+            next unless $line;
+            chomp($line);
+            my @tmp = line_parse($line);
+            next unless @tmp;
+
+            unless (@buffer) {
+                @buffer = @tmp;
+                $line   = <$fh>;
+                redo;
+            }
+
+            if ($tmp[0] eq $buffer[0]) {
+                push @res, [@buffer];
+                @buffer = @tmp;
+            } else {
+                push @res, [@buffer];
+                @buffer = @tmp;
+                return @res;
             }
         }
-    }
+        return @res;
+      }
+}
 
-    if ($bootstrap) {
-        return;
-    } else {
-        return \%changed, \%dropped;
+sub extract {
+    my ($ary, $type) = @_;
+
+    return join '',
+      map { $_->[2] }
+      grep { $_->[1] eq $type } sort { $a->[2] cmp $b->[2] } @$ary;
+}
+
+sub compare {
+    my ($n, $o) = @_;
+    my $name = $n->[0][0];
+
+    my $nns = extract($n, 'NS');
+    my $ons = extract($o, 'NS');
+    my $nds = extract($n, 'DS');
+    my $ods = extract($o, 'DS');
+    my $na  = extract($n, 'A');
+    my $oa  = extract($o, 'A');
+
+    $changed{$name} .= 'NS ' if $nns ne $ons;
+    $changed{$name} .= 'DS ' if $nds ne $ods;
+    $changed{$name} .= 'A '  if $na  ne $oa;
+
+    my %oldns = map { $_->[2], 1 } grep { $_->[1] eq 'NS' } @$o;
+    foreach my $new (map { $_->[2], 1 } grep { $_->[1] eq 'NS' } @$n) {
+        delete $oldns{$new};
+    }
+    foreach my $ns (keys %oldns) {
+        $dropped{$name}{$ns} = 1;
     }
 }
 
-sub norm {
-    my $r = shift;
+sub process {
+    my $new = get_iterator($filename . $newsuffix);
+    my $old = get_iterator($filename);
+    my @new = $new->();
+    my @old = $old->();
 
-    if ($r) {
-        return join '', sort @$r;
-    } else {
-        return '';
+    while (@new or @old) {
+        my $n;
+        my $o;
+
+        $n = $new[0][0] if @new;
+        $o = $old[0][0] if @old;
+
+        if ($n and $flagdomain{exist}{$n}) {
+            $flagdomain{new}{$n} = 1;
+        }
+        if ($o and $flagdomain{exist}{$o}) {
+            $flagdomain{old}{$o} = 1;
+        }
+
+        if ($n eq $o) {
+            compare(\@new, \@old);
+            @new = $new->();
+            @old = $old->();
+        } elsif ($n gt $o) {
+            @old = $old->();
+        } elsif ($n lt $o) {
+            $added{$n} = 1;
+            @new = $new->();
+        } else {
+            print "WTF: $n $o\n";
+            last;
+        }
     }
 }
 
 sub get_source_id {
-    my $dc  = shift;
     my $dbh = $dc->dbh;
 
     $dbh->do(q[INSERT IGNORE INTO source (name) VALUES (?)],
-        undef, $dc->config->get("zonediff")->{sourcestring});
+        undef, $config->{sourcestring});
     my @res = $dbh->selectrow_array(q[SELECT id FROM source WHERE name = ?],
-        undef, $dc->config->get("zonediff")->{sourcestring});
+        undef, $config->{sourcestring});
 
     print "Got source id " . $res[0] . "\n" if $debug;
     return $res[0];
 }
 
-GetOptions("bootstrap" => \$bootstrap, "debug" => \$debug);
+sub save_to_database {
+    my $dbh       = $dc->dbh;
+    my $source_id = get_source_id();
 
-my $dc        = DNSCheck->new;
-my $source_id = get_source_id($dc);
-my $sth =
-  $dc->dbh->prepare(
+    my $drop_sth = $dbh->prepare(
+q[INSERT IGNORE INTO delegation_history (domain, nameserver) VALUES (?,?)]
+    );
+    my $queue_sth = $dbh->prepare(
 q[INSERT INTO queue (priority,domain,source_id,source_data) VALUES (?,?,?,?)]
-  );
-my $drop_sth = $dc->dbh->prepare(
-    q[INSERT IGNORE INTO delegation_history (domain, nameserver) VALUES (?,?)]);
+    );
 
-my ($changed, $dropped) = get_changed_domains($dc->config->get("zonediff"));
+    foreach my $name (keys %added) {
+        $queue_sth->execute(3, $name, $source_id, 'NEW');
+        print "Queue: $name NEW\n" if $debug;
+    }
 
-foreach my $domain (keys %$changed) {
-    printf("Queueing zone %s for test because of %s.\n",
-        $domain, $changed->{$domain})
-      if $debug;
-    $sth->execute(3, $domain, $source_id, $changed->{$domain});
-}
+    foreach my $name (keys %changed) {
+        $queue_sth->execute(3, $name, $source_id, $changed{$name});
+        print "Queue: $name " . $changed{$name} . "\n" if $debug;
+    }
 
-while (my ($d, $v) = each %$dropped) {
-    foreach my $n (keys %{$v}) {
-        $n =~ s/\.$//;
-        printf("Inserting %s for %s into delegation history.\n", $n, $d);
-        $drop_sth->execute($d, $n);
+    foreach my $zone (keys %dropped) {
+        foreach my $ns (keys %{ $dropped{$zone} }) {
+            $drop_sth->execute($zone, $ns);
+            print "Delegation History: $zone $ns\n" if $debug;
+        }
     }
 }
 
+setup();
+fetch_new_zone();
+process();
+check_for_flagdomains();
+save_to_database();
+switch_files_around();
+
 =head1 NAME
 
-dnscheck-zonediff - Fetch a zone with dig and schedule tests for those changed
+dnscheck-zonediff - Tool to preiodically detect changes in a zone
 
-=head1 DESCRIPTION
+=head1 SYNOPSIS
 
-This program does a zone transfer of an entire domain, accumulates the NS and
-DS records for each name in it, looks at the sorted and
-concatenated string representations of the records and sees if they are the
-same as it was the last time the script was run. Any domain for which they
-are not the same is entered into the C<queue> table in the L<DNSCheck>
-database.
+dnscheck-zonediff [--debug]
 
-It also looks at NS records, and adds those that exist in the saved file from
-the last run but not in the current run to the database table
-C<delegation_history>.
+Options:
 
-This program is intended to be executed regularly from L<cron> or a similar
-scheduler.
+ --debug               Make the script print out some progress and result information.
 
-=head1 CONFIGURATION
+=head1 EXPLANATION
 
-This program gets all its configuration from the same YAML files as the rest
-of the L<DNSCheck> system. It looks for its data under the key C<zonediff>. It
-looks for six subkeys:
+This script calls out to L<dig> to download a zonefile using a cryptographic
+signature. The zone, the signature, the servers to ask, the file to save the
+data in, the database to put insert differences into and so on is all
+specified in L<config.yaml> (or the corresponding site_config).
 
-=over
-
-=item tsig
-
-The signature to be used to authorise the transfer, if one is needed. Should
-be in a format that can be fed dirctly to L<Net::DNS::RR::new> (which is for
-practical purposes the same as you'd put in a zone file: "keyname TSIG
-keydata"). If this key is set to an empty value, TSIG will not be used.
-
-=item datafile
-
-The full path to the file where zone names and record contents will be stored
-between runs.
-
-=item servers
-
-A list of servers to try to do the zone transfer from. They will be tried in
-the order listed.
-
-=item flagdomain
-
-A list of special domain names thats will be present if the entire zone was
-correctly transferred. If one or more of the listed domains are missing after
-the transfer has concluded, it will be assumed that the transfer was
-incomplete and a new attempt will be made with the next server. If there are
-no more servers to try, the script will exit with an error message. If this
-key is not present, the check will not be made.
-
-The file with saved data from a previous run will also be checked for the
-existence of the flag domains, and the run terminated if they're not there. In
-order to initally populate a save file, or after the list of flag domains has
-been changed, run the program with the C<--bootstrap> flag.
-
-=item domain
-
-The domain to check.
-
-=item sourcestring
-
-The string used to mark tests queued from this script.
-
-=item dig
-
-The full path to the L<dig> binary to use for zone transfers.
-
-=back
-
+This script relies heavily on the output from L<dig> nor changing its format
+and to be in properly sorted order.
