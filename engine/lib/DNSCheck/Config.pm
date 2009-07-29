06399ef505fcafd821622c7d4f2fb87e160d4430
Calle Dybedahl <calle@init.se>
2009-07-29 07:44:32 +0000

    Backport fix for #113.

--- engine/lib/DNSCheck/Config.pm
+++ engine/lib/DNSCheck/Config.pm
@@ -43,6 +43,7 @@ use Carp;
 use Cwd;
 use FindBin;
 use List::Util qw(first);
+use Storable qw(dclone);
 
 sub new {
     my $proto = shift;
@@ -118,6 +119,10 @@ sub get {
     my ($key) = @_;
 
     my $res = $self->{$key};
+    if (ref($res)) {
+        $res = dclone($res);
+    }
+    
     return $res;
 }
 
