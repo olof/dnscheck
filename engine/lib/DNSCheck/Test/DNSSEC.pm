3217e0ba62abf412a42c6512c62d91c1794528f3
Calle Dybedahl <calle@init.se>
2010-05-11 12:54:44 +0000

    Verify that a DS correctly signs a DNSKEY.

--- engine/lib/DNSCheck/Test/DNSSEC.pm
+++ engine/lib/DNSCheck/Test/DNSSEC.pm
@@ -381,7 +381,9 @@ sub _check_parent {
 
         # REQUIRE: the DS MUST point to a DNSKEY that is
         # signing the child's DNSKEY RRset
-        if (_count_in_list($rr->keytag, $child_result->{anchors}) >= 1) {
+        if (_count_in_list($rr->keytag, $child_result->{anchors}) >= 1
+            and $rr->verify($child_result->{rr}{ $rr->keytag }))
+        {
             ## DS refers to key signing the DNSKEY RRset
             $logger->auto("DNSSEC:DS_KEYREF_OK", $zone, $ds_message);
         } else {
