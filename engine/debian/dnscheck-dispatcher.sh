9fcb0b29287a798cd843da753449e46ea8ab101e
Olof Johansson <olof@ethup.se>
2012-12-05 20:23:26 +0100

    Dependency based booting for dispatcher initscript

--- engine/debian/dnscheck-dispatcher.sh
+++ engine/debian/dnscheck-dispatcher.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          dnscheck-dispatcher
+# Required-Start:    $remote_fs $syslog $network mysql
+# Required-Stop:     $remote_fs $syslog $network mysql
+# Default-Start:
+# Default-Stop:
+# Short-Description: Start/stop the DNSCheck dispatcher
+### END INIT INFO
 
 start() {
     /usr/bin/dnscheck-dispatcher
